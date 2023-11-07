0807d36aa976740d7a592f1c17ca18b8279405bf
Artem Petrikov <112977031+ManForTen@users.noreply.github.com>
2023-11-07 19:37:06 +0300

    ПИ21-4 Петриков Артём

--- src/App.js
+++ src/App.js
@@ -1,95 +1,69 @@
+import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import {useState} from "react"; // Состояние
 
-// компонент grid столбик
-function GridObject({image, text}) {
-    return (
-        <>
-            <img src={image} alt="image"></img>
-            <p className={"text"}>{text}</p>
-        </>
-    )
-}
 
-// компонент ссылки в хедере
-function HeaderPunkt({ classname, name }) {
-    return (
-        <a href={'.'} className={classname}>{name}</a>
-    );
-}
+const dataArray = [ // Объект
+    {imgSrc: '1.jpg', name: 'Мясной пир', description: 'Очень вкусный пир!' }, // Внутри свойства(props)
+    {imgSrc: '2.jpg', name: 'Брускетта с пастрами', description: 'Очень вкусные брускетты!' },
+    {imgSrc: '3.jpg', name: 'Джерки собственного производства', description: 'Это наше производство!' },
+    {imgSrc: '4.jpg', name: 'Специалитет плато', description: 'Очень вкусное блюдо!' },
+    {imgSrc: '5.jpg', name: 'Антипасти', description: 'Как паста, только анти' },
+    {imgSrc: '6.jpg', name: 'Сырное плато', description: 'Оочень много сыра!' },
+];
 
-// компонент кнопки с хуками
-function Button({id, image, text, text2}) {
-    // использование хука (переменная, которая меняется и функция, которая меняет
-    const [showComponent, setShowComponent] = useState(false);
-    // функция, которая вызывается в компоненте, она меняет его состояние
-    const toggleComponent = () => {
-        setShowComponent(!showComponent);
+function generateElements(dataArray) { // Функциональный компонент
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [showDescriptionIndex, setShowDescriptionIndex] = useState(false); // Хук
+
+    const handleShowDescription = (index) => {
+        setShowDescriptionIndex(index === showDescriptionIndex ? false : index);
     };
 
-    return (
-        <>
-            <button id={id} onClick={toggleComponent}>
-                {showComponent ? <div id={id}><GridObject image={image} text={text}></GridObject></div>
-                    : <div id={id}><GridObject image={image} text={text2}></GridObject></div>}
+    return dataArray.map((item, index) => (
+        <div key={index} className="scale">
+            <br></br>
+            <img src={item.imgSrc} alt={item.name} />
+            <p>{item.name}</p>
+            {showDescriptionIndex === index ? <p className="description">{item.description}</p> : false}
+            <button onClick={() => handleShowDescription(index)}>
+                Подробнее
             </button>
-        </>
-    )
-}
-// использование объектов со свойствами (props)
-const arrayheader = [
-    {name: 'Гурманика', classname: 'componyname'},
-    {name: 'доставка', classname: 'e4_14'},
-    {name: 'о нас', classname: 'e4_18'},
-    {name: 'КОРЗИНА', classname: 'e4_42'},
-    {name: 'рестораны', classname: 'e4_17'},
-    {name: 'акции', classname: 'e4_16'},
-];
+        </div>
 
-const grid1 = [
-    {gridid: 'item-0', imgSrc: 'image_4.png', text: 'Шашлык куриный 90 гр 165 руб', text2: 'Шашлык куриный'},
-    {gridid: 'item-1',imgSrc: 'image_5.png', text: 'Картофель жареный с грибами 200 гр 240 руб', text2: 'Картофель жареный с грибами' },
-    {gridid: 'item-2',imgSrc: 'image_6.png', text: 'Куриная грудка су-вид 200 гр 340 руб', text2: 'Куриная грудка су-вид' }
-];
-
-const grid2 = [
-    {gridid: 'item-0', imgSrc: 'image_7.png', text: 'Салат Греческий 200 гр 310 руб', text2: 'Салат Греческий'},
-    {gridid: 'item-1',imgSrc: 'image_8.png', text: 'Булочка с маком 200 гр 150 руб', text2: 'Булочка с маком' },
-    {gridid: 'item-2',imgSrc: 'image_9.png', text: 'Морс клюква-брусника 500 гр 175 руб', text2: 'Морс клюква-брусника ' }
-];
-
-// компонент генератора хедера со стролочной функцией
-function HeaderGenerator({array}) {
-    // стрелочная функция по списку с использованием реквизитов
-    return array.map((item) => (
-        <HeaderPunkt name={item.name} classname={item.classname}></HeaderPunkt>
     ));
 }
-// компонент grid столбик
-function GridGenerator({array}) {
-    // стрелочная функция по списку с использованием реквизитов
-    return array.map((item) => (
-            <Button id={item.gridid} image={item.imgSrc}
-                    text={item.text} text2={item.text2}></Button>
-    ));
+
+function ComponentMenu() { // Функциональный компонент
+    return <div className="angry-grid">{generateElements(dataArray)}</div>;
 }
+
 
 function App() {
     return (
-        <>
-            {/* в качестве аргументов передаются свойства */}
-            <div className="panel"></div>
-            <a href={'.'} className="logo"></a>
-              <HeaderGenerator array={arrayheader}></HeaderGenerator>
-              <a href={'.'} className="account"></a>
-              <div className="angry-grid">
-                  <GridGenerator array={grid1}></GridGenerator>
-              </div>
-              <div className="angry-grid">
-                  <GridGenerator array={grid2}></GridGenerator>
-              </div>
-            <div className="e4_22"></div>
-        </>
+    <div className="App">
+        <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-black">
+            <a className="navbar-brand text-white namerest" href="#">Ресторан RUN</a>
+            <button className="navbar-toggler but" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link text-white" href="#">Главная страница <span className="sr-only"></span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link text-white" href="#">Блюдо дня</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link text-white" href="#">Скидка века</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <ComponentMenu></ComponentMenu>
+    </div>
   );
 }
 
